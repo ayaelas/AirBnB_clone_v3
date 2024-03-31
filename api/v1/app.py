652675655f10c0f9aaf53f396875fc479cf0aa33
ayaelas <assalharras@gmail.com>
2024-03-31 17:44:27 +0000

    api

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,27 +1,28 @@
 #!/usr/bin/python3
-
 """Api"""
+
+from api.v1.views import app_views
 from os import getenv
 from flask import Flask
 from flask import jsonify
 from flask_cors import CORS
 from models import storage
-from api.v1.views import app_views
 
 app = Flask(__name__)
 CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 app.url_map.strict_slashes = False
 app.register_blueprint(app_views)
 
-@app.errorhandler(404)
-def notfound(error):
-    """404 Error  Not found"""
-    return jsonify({"error": "Not found"}), 404
 
 @app.teardown_appcontext
 def app_teardown(exception):
-    """Closes the storage session"""
+    """Close section"""
     storage.close()
+
+@app.errorhandler(404)
+def notfound(error):
+    """Handles 404 Errors"""
+    return jsonify({"error": "Not found"}), 404
 
 if __name__ == "__main__":
     app.run(
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+from flask import jsonify
+from models import storage
 from api.v1.views import app_views
 
 @app_views.route("/status")
