261d6be80a1cdefbcf4044c8661e5d0e7b23a5db
youssef266 <youssef.toti.taha.1212@gmail.com>
2024-03-31 20:01:59 +0200

    fix task 3

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,28 +1,20 @@
 #!/usr/bin/python3
-"""Api"""
-
-from api.v1.views import app_views
-from os import getenv
+""" API v1 """ 
 from flask import Flask
-from flask import jsonify
 from flask_cors import CORS
+from os import getenv
 from models import storage
+from api.v1.views import app_views
 
 app = Flask(__name__)
 CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 app.url_map.strict_slashes = False
 app.register_blueprint(app_views)
 
-
 @app.teardown_appcontext
 def app_teardown(exception):
-    """Close section"""
+    """ Closes the storage """
     storage.close()
-
-@app.errorhandler(404)
-def notfound(error):
-    """Handles 404 Errors"""
-    return jsonify({"error": "Not found"}), 404
 
 if __name__ == "__main__":
     app.run(
