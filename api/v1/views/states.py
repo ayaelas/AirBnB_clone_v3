b92c285f93a0068089b9fe0e8ded1d041581a39f
youssef266 <youssef.toti.taha.1212@gmail.com>
2024-04-01 07:58:53 +0200

    last fix

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -4,7 +4,6 @@ from models.state import State
 from models import storage
 from api.v1.views import app_views
 from flask import abort, jsonify, make_response, request
-from flasgger.utils import swag_from
 
 """
 objects that handle all default RestFul
@@ -14,7 +13,6 @@ GET Method, Retrieves the list of all State objects
 
 
 @app_views.route('/states', methods=['GET'], strict_slashes=False)
-@swag_from('documentation/state/get_state.yml', methods=['GET'])
 def get_states():
     """
     Retrieves the list of all State objects
@@ -34,7 +32,6 @@ GET Method, Retrieves a specific State
 
 
 @app_views.route('/states/<state_id>', methods=['GET'], strict_slashes=False)
-@swag_from('documentation/state/get_id_state.yml', methods=['get'])
 def get_state(state_id):
     """ Retrieves a specific State """
     state = storage.get(State, state_id)
@@ -46,7 +43,6 @@ def get_state(state_id):
 
 @app_views.route('/states/<state_id>', methods=['DELETE'],
                  strict_slashes=False)
-@swag_from('documentation/state/delete_state.yml', methods=['DELETE'])
 def delete_state(state_id):
     """
     Deletes a State Object
@@ -71,7 +67,6 @@ POST Method, Creates a State
 
 
 @app_views.route('/states', methods=['POST'], strict_slashes=False)
-@swag_from('documentation/state/post_state.yml', methods=['POST'])
 def post_state():
     """
     Creates a State
@@ -96,7 +91,6 @@ PUT Method, Updates a State
 
 
 @app_views.route('/states/<state_id>', methods=['PUT'], strict_slashes=False)
-@swag_from('documentation/state/put_state.yml', methods=['PUT'])
 def put_state(state_id):
     """
     Updates a State
